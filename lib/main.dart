7549b0ebaaae89703aa487d75bca1d357f1ad7f8
oxunov_713 <aoxunov713@gmail.com>
2023-08-15 09:41:34 +0500

    Oltinchi rasm

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
-          color: Colors.grey,
+          color: Colors.white,
           child: Center(
             child: Column(
               children: [
@@ -14,119 +14,81 @@ void main() {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      margin: EdgeInsets.only(left: 15, top: 40),
+                      margin: EdgeInsets.only(left: 15, top: 80),
                       child: Text(
-                        "#364649",
-                        style: TextStyle(fontSize: 22, color: Colors.white70),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 15, top: 40, right: 15),
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Color(0xFF364649),
+                        "Gradient Inspiration",
+                        style: TextStyle(fontSize: 30, color: Colors.blueGrey),
                       ),
                     ),
                   ],
                 ),
-                Container(
-                  margin: EdgeInsets.only(top: 30),
-                  height: 150,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    gradient: LinearGradient(colors: [
-                      Color(0xFF364649),
-                      Color(0xFF708F96),
-                    ], begin: Alignment.topRight, end: Alignment.bottomLeft),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 8, top: 150, left: 69),
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFE21C34),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 15, top: 150),
+                          child: Text(
+                            "#E21C34",
+                            style:
+                                TextStyle(fontSize: 30, color: Colors.blueGrey),
+                          ),
+                        ),
+                      ],
+                    ),
                     Container(
-                      margin: EdgeInsets.only(right: 8, top: 30, left: 5),
-                      height: 30,
-                      width: 30,
+                      margin: EdgeInsets.only(top: 30),
+                      height: 250,
+                      width: 250,
                       decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Color(0xFF708F96),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.shade700,
+                              blurRadius: 15,
+                              spreadRadius: 5,
+                              offset: Offset(15, 20)),
+                        ],
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFE21C34),
+                              Color(0xFF500B28),
+                            ],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight),
                       ),
                     ),
-                    Container(
-                      margin: EdgeInsets.only(left: 15, top: 30),
-                      child: Text(
-                        "#708F96",
-                        style: TextStyle(fontSize: 22, color: Colors.white70),
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 30),
-                  height: 150,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    gradient: LinearGradient(colors: [
-                      Color(0xFF364649),
-                      Color(0xFFAA895F),
-                    ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 15, top: 30),
-                      child: Text(
-                        "#AA895F",
-                        style: TextStyle(fontSize: 22, color: Colors.white70),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 15, top: 30, right: 15),
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Color(0xFFAA895F),
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 30),
-                  height: 150,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    gradient: LinearGradient(colors: [
-                      Color(0xFFAA895F),
-                      Color(0xFFE0D8CC),
-                    ], begin: Alignment.topRight, end: Alignment.bottomLeft),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(right: 8, top: 30, left: 5),
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Color(0xFFE0D8CC),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 15, top: 30),
-                      child: Text(
-                        "#E0D8CC",
-                        style: TextStyle(fontSize: 22, color: Colors.white70),
-                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 30, right: 10),
+                          child: Text(
+                            "#500B28",
+                            style:
+                                TextStyle(fontSize: 30, color: Colors.blueGrey),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 15, top: 30, right: 69),
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF500B28),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
