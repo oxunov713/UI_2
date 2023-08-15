cd43d71af2eb41f8e109e63db75d1eb908e66f6d
oxunov_713 <aoxunov713@gmail.com>
2023-08-15 08:52:12 +0500

    Birinchi rasm done

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,117 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text("Tony"),
+          child: Container(
+            height: double.infinity,
+            width: 300,
+            margin: EdgeInsets.only(top: 10, bottom: 10),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 30),
+                  child: Text(
+                    "Select your avatar",
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 150,
+                  width: 150,
+                  margin: EdgeInsets.only(top: 30),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.orange,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          height: 90,
+                          width: 90,
+                          margin: EdgeInsets.only(
+                              top: 50, left: 20, right: 20, bottom: 20),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.red,
+                          ),
+                        ),
+                        Container(
+                          height: 90,
+                          width: 90,
+                          margin: EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.green,
+                          ),
+                        ),
+                        Container(
+                          height: 90,
+                          width: 90,
+                          margin: EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.pink,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          height: 90,
+                          width: 90,
+                          margin: EdgeInsets.only(
+                              top: 50, left: 20, right: 20, bottom: 20),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.indigo,
+                          ),
+                        ),
+                        Container(
+                          height: 90,
+                          width: 90,
+                          margin: EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.purple,
+                          ),
+                        ),
+                        Container(
+                          height: 90,
+                          width: 90,
+                          margin: EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.pinkAccent,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 30),
+                  height: 60,
+                  width: 250,
+                  child: Center(
+                    child: Text(
+                      "Select",
+                      style: TextStyle(fontSize: 30,color: Colors.white),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     ),
